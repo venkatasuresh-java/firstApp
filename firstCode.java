a127f01085f4913a8ed7deb0401581849e6e3160
suresh <venkatasuresh.gunja.java@gmail.com>
2026-02-12 18:06:35 +0530

    Third  commit

--- firstCode.java
+++ firstCode.java
@@ -4,5 +4,9 @@ class firstCode{
   
     public static void main(String[] args) {
         System.out.println("Hai Java");
+        int a =1;
+        int b = 2;
+        int num = a+b;
+        System.out.println(num);
     }
 }
